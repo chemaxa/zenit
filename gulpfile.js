a551c1ab8805360c26cdff7dc1039dc5f4f0a801
Maxim Shkarbalyuk <chemaxa@gmail.com>
2017-03-22 22:26:43 +0300

    Small fixes

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ let concat = require('gulp-concat');
 let rename = require('gulp-rename');
 let csso = require('gulp-csso');
 let sourcemaps = require('gulp-sourcemaps');
+let runSequence = require('run-sequence');
 
 let params = {
     scssPaths: ['./node_modules', './src/scss/blocks', './src/scss/pages'],
@@ -19,20 +20,11 @@ let params = {
 };
 console.info('CurrentPage: ', params.currentPage);
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['clean', 'fonts', 'images', 'pug', 'scss', 'js'], function () {
-    browserSync.init({
-        server: {
-            baseDir: "./" + params.build,
-            index: "/html/" + params.currentPage + ".html"
-        }
-    });
-    gulp.watch(params.src + "/pug/**/*.pug", ['pug']);
-    gulp.watch(params.src + "/scss/**/*.scss", ['scss']);
-});
+
+
 
 // Compile scss into CSS & auto-inject into browsers
-gulp.task('scss', ['clean'], function () {
+gulp.task('scss', function () {
     return gulp.src(params.src + "/scss/main.scss")
         .pipe(sourcemaps.init())
         .pipe(scss({
@@ -48,7 +40,7 @@ gulp.task('scss', ['clean'], function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('pug', ['clean'], function () {
+gulp.task('pug', function () {
     return gulp.src(params.src + '/pug/pages/' + params.currentPage + '.pug')
         .pipe(pug({
             pretty: true,
@@ -60,18 +52,19 @@ gulp.task('pug', ['clean'], function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('js', ['clean'], function () {
+gulp.task('js', function () {
     return gulp.src(['./node_modules/jquery/dist/jquery.js', params.src + '/js/**/*'])
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest(params.build + '/js'))
+        .pipe(browserSync.stream());
 })
 
-gulp.task('images', ['clean'], function () {
+gulp.task('images', function () {
     return gulp.src(params.src + '/img/**/*')
         .pipe(gulp.dest(params.build + '/img'))
 })
 
-gulp.task('fonts', ['clean'], function () {
+gulp.task('fonts', function () {
     return gulp.src(params.src + '/fonts/**/*')
         .pipe(gulp.dest(params.build + '/fonts'))
 })
@@ -83,5 +76,23 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
-gulp.task('build', ['clean', 'scss', 'pug', 'fonts', 'images', 'js']);
+gulp.task('build', function (done) {
+    runSequence('clean', 'scss', 'pug', 'fonts', 'images', 'js', done)
+});
+// Static Server + watching scss/html files
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./" + params.build,
+            index: "/html/" + params.currentPage + ".html"
+        }
+    });
+    gulp.watch(params.src + "/js/**/*.js", ['js']);
+    gulp.watch(params.src + "/pug/**/*.pug", ['pug']);
+    gulp.watch(params.src + "/scss/**/*.scss", ['scss']);
+}
+gulp.task('serve', function () {
+    runSequence('clean', 'scss', 'pug', 'fonts', 'images', 'js', serve)
+});
+
 gulp.task('default', ['serve']);
