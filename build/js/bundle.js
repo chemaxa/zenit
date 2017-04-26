10212bd53ed2655edb3df7d3f4a97895a348621f
Maxim Shkarbalyuk <chemaxa@gmail.com>
2017-04-26 21:07:17 +0300

    Fixed menu

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -9819,6 +9819,7 @@ $(function () {
   $('[data-toggler]').click(function (event) {
     var toggleClass = $(this).data('toggle-class');
     var target = $(this).data('toggle');
+    console.log($(target));
     $(target).toggleClass(toggleClass);
   });
 });
